0b29b202d3d3849383fb9c0da286459bf3e63a2f
Diablo <122763759+Diablo2009@users.noreply.github.com>
2024-09-16 18:16:02 -0600

    Update QEMU.sh

--- tools/Emulation/QEMU.sh
+++ tools/Emulation/QEMU.sh
@@ -3,14 +3,18 @@
 ## Variables / Constants
 BOXES=/home/deck/.local/share/flatpak/exports/bin/org.gnome.Boxes
 BIN=/usr/bin/flatpak
-URL=""
+URL="https://raw.githubusercontent.com/Diablo2009/SteamDeck/main/tools/Emulation/.bashrc"
+LOC=$0
 
 ## Script
 if [[ -f $BOXES ]]; then
-  wget $URL
+  echo "We are replacing your bashrc file, if you have edited that file, please back it up"
+  sleep 10
+  wget -o /home/deck/.bashrc $URL
 else
   echo "Starting Flatpak Install"
   sleep 5
   clear
   $BIN install org.gnome.Boxes
+  bash $0
 fi
